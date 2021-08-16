c1b0573d7d249e36f819812edfe1bca464456819
Dallas <dallas.clarke622@gmail.com>
2021-08-16 00:32:53 -0700

    Made some optimizations, like fixing issue with city searches & returning definitions of acronymns.

--- app1.py
+++ app1.py
@@ -8,6 +8,10 @@ def translate(w):
 
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
@@ -22,4 +26,10 @@ def translate(w):
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = (translate(word))
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
